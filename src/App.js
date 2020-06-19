61aed2ecfb140ef64ae4455f0e1e49a02bda4791
eugene-finn <1287175@gmail.com>
2020-06-19 16:29:00 +0300

    auth template

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./Header";
 import SearchInput from "./SearchInput";
 import EmojiResults from "./EmojiResults";
 import filterEmoji from "./filterEmoji";
+import "./App.css";
+
+import firebase from "firebase";
+import "firebase/auth";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+
+firebase.initializeApp({
+  apiKey: `${process.env.REACT_APP_FIREBASE_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}`
+});
 
 const App = () => {
+  const [isSignIn, setSignIn] = useState(false);
+
+  const uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID
+    ],
+    callbacks: { signInSuccessWithAuthResult: () => false }
+  };
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged(user => {
+      setSignIn(!!user);
+    });
+  }, []);
+
   const [filteredEmoji, setfilteredEmoji] = useState(filterEmoji("", 20));
 
   const handleSearchChange = event => {
@@ -13,6 +40,19 @@ const App = () => {
 
   return (
     <>
+      <div>
+        {isSignIn ? (
+          <div className="authentication">
+            <img src={firebase.auth().currentUser.photoURL} alt="profile pic" />
+            <span className="welcome">Welcome {firebase.auth().currentUser.displayName}!</span>
+            <button onClick={() => firebase.auth().signOut()}>Sign Out!</button> <br />
+          </div>
+        ) : (
+          <div>
+            <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+          </div>
+        )}
+      </div>
       <Header />
       <SearchInput textChange={event => handleSearchChange(event)} />
       <EmojiResults emojiData={filteredEmoji} />
